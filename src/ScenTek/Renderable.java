e856fb48822a92f10e58ad919dfdb453e489abd5
aaldrich <Andrew1Aldrich@hotmail.com>
2015-03-19 16:57:45 -0400

    Created Renderable interface

---
+++ src/ScenTek/Renderable.java
@@ -0,0 +1,15 @@
+
+
+package ScenTek;
+
+/**
+ * A Renderable will be able to render itself onscreen
+ * @author Andrew
+ */
+public interface Renderable {
+    /**
+     * instructs the Renderable to render itself on screen
+     */
+    public void render();
+    
+}
