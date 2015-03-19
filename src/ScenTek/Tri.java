2ff1fe6603641999b336232706dc7c653d031a6e
aaldrich <Andrew1Aldrich@hotmail.com>
2015-03-18 22:21:33 -0400

    Created Tri class
    awaiting matrix stuff for translation, rotation, and dilation

---
+++ src/ScenTek/Tri.java
@@ -0,0 +1,58 @@
+
+package ScenTek;
+
+import ScenTek.geom.Point;
+import javax.naming.OperationNotSupportedException;
+
+/**
+ * Contains 3 {@link Point} for the purpose of rendering
+ * @author Andrew
+ */
+class Tri {
+    private Point[] points;
+    /**
+     * Creates the Tri from the 3 passed points
+     * @param points 
+     */
+    public Tri(Point[] points){
+        if(points.length > 3){
+            throw new IllegalArgumentException("More than 3 Points passed");
+        }
+        if(points.length < 3){
+            throw new IllegalArgumentException("Less than 3 Points passed");
+        }
+        this.points = points;
+    }
+    /**
+     * Takes 3 points as parameters and calls the array constructor
+     * @param p1
+     * @param p2
+     * @param p3 
+     */
+    public Tri(Point p1, Point p2, Point p3){
+        this(new Point[]{p1, p2, p3});
+    }
+    /**
+     * Creates a Tri from an array in a {x1,y1,x2,y2,x3,y3} format 
+     * @param points coordinates stored as {x1,y1,x2,y2,x3,y3} 
+     */
+    public Tri(float[] points){
+        this(new Point[]{
+            new Point(points[0], points[1]),
+            new Point(points[2], points[3]),
+            new Point(points[4], points[5])});
+    }
+    
+    public void translate(float dx, float dy) throws OperationNotSupportedException{
+        throw new OperationNotSupportedException("Awaiting matrix stuff");
+    }
+    
+    public void rotate(float dRadians) throws OperationNotSupportedException{
+        throw new OperationNotSupportedException("Awaiting matrix stuff");
+    }
+    
+    public void scale(float magnitude) throws OperationNotSupportedException{
+        throw new OperationNotSupportedException("Awaiting matrix stuff");
+    
+    }
+}
