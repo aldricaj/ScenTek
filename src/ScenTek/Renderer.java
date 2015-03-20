c77ab574c4141285160065bc5c662ad0be5c9807
aaldrich <Andrew1Aldrich@hotmail.com>
2015-03-20 16:13:41 -0400

    Changed Renderer to interface and added methods
    Renderer is now an interface that will be passed in the constructor of
    any renderer.

--- src/ScenTek/Renderer.java
+++ src/ScenTek/Renderer.java
@@ -2,9 +2,15 @@
 package ScenTek;
 
 /**
- * This class will handle rendering
+ * This interface will handle rendering
  * @author aaldrich
  */
-public abstract class Renderer {
-
+public interface Renderer {
+    /**
+     * Renders the passed points using Triangles made from the passed indices
+     * @param pts the points to be rendered. In order in clockwise order
+     * @param indices indices in the pts array that creates triangles
+     */
+    public void render(float[][] pts, int[] indices);
+    
 }
