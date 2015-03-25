8a8b2897d055d657dc734c82905b1aa4b564f4e6
aldrich_a <aldrich_a@WH7010Rm243-17.llsdcompscience.internal>
2015-03-25 10:47:06 -0400

    Finish later

--- src/ScenTek/render/Renderer.java
+++ src/ScenTek/render/Renderer.java
@@ -1,16 +1,27 @@
 
-package ScenTek;
+package ScenTek.render;
 
 /**
  * This interface will handle rendering
  * @author aaldrich
  */
-public interface Renderer {
+public class Renderer {
+    private Renderer instance;
+    private Renderer(){
+        
+    }
+    
     /**
      * Renders the passed points using Triangles made from the passed indices
      * @param pts the points to be rendered. In order in clockwise order
      * @param indices indices in the pts array that creates triangles
      */
-    public void render(float[][] pts, int[] indices);
+    public void render(float[][] pts, int[] indices, ShaderProgram shader){
+        
+    }
+    
+    public Renderer getInstance(){
+        return (instance == null) ? instance : (instance = new Renderer());
+    }
     
 }
