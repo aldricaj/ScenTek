fd2d85afa046072be6347889b340527953a20d9a
aaldrich <Andrew1Aldrich@hotmail.com>
2015-04-03 01:52:18 -0400

    Change in design
    Renderer will now be an abstract class

--- src/ScenTek/render/Renderer.java
+++ src/ScenTek/render/Renderer.java
@@ -1,26 +1,42 @@
 
 package ScenTek.render;
 
+import static org.lwjgl.opengl.GL11.*;
+
 /**
  * This interface will handle rendering
  * @author aaldrich
  */
-public class Renderer {
-    
-    private Renderer instance;
-    private Renderer(){
+public abstract class Renderer {
+    protected int vao = -1;// allows subclass to determine whether or not it has a created vao
+    protected float[] points;
+    protected byte[] indices;
+    protected Renderer(){
         
     }
     
-    public void render(float[][] pts, int[] indices){
-        
-    }
     /**
-     * Get an instance of the renderer
-     * @return current instance
+     * Load the necessary settings for the render type
+     
+     * Should contain all initialization for whatever rendering process is needed by the subclass. It'll set up VAO mainly, using the id assigned
      */
-    public Renderer getInstance(){
-        return (instance == null) ? (instance = new Renderer()) : (instance); // ensure that instance has been created, if not, make one
+    protected abstract void loadSettings();
+    /**
+     * Set everything modified to it's default value
+     */
+    protected abstract void unloadSettings();
+    
+    /**
+     * Render at the points and the indices passed
+     * @param pts points to be rendered at
+     * @param indices indices for the triangles to be used
+     */
+    public void render(float[] pts, byte[] indices){
+        points = pts;
+        this.indices = indices;
+        loadSettings();
+        glDrawElements(GL_TRIANGLES, indices.length, GL_UNSIGNED_BYTE, 0);
+        unloadSettings();
     }
     
     
