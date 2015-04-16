7b3ccdba2043ac260dc2ba45e65fecb6a588ea91
aldrich_a <aldrich_a@WH7010Rm243-17.llsdcompscience.internal>
2015-04-16 10:32:02 -0400

    Added Javadoc and modified run method to be called by a child class. The update method also requires a double to allow the elapsed time to be passed.

--- src/ScenTek/Game.java
+++ src/ScenTek/Game.java
@@ -14,47 +14,55 @@ import org.lwjgl.opengl.GLContext;
  * @author aaldrich
  */
 public abstract class Game extends GLFWKeyCallback{
-    // We need to strongly reference callback instances.
+    /**Gives ability to access errors*/
     private GLFWErrorCallback errorCallback;
-    private GLFWKeyCallback   keyCallback;
-    private final int fps_limit = 800;
+    /**the fps limit of the game*/
+    private final int fps_limit = 30;
+    /**The label of the window*/
     private String gameName;
+    /**The currently displayed window*/
     private Window window;
-    
+    /**the current working directory*/
     public static final String WORKING_DIR = System.getProperty("user.dir");
-    
+    /**
+     * Creates a game with the label passed
+     * @param name the label for the game window
+     * @param windowed whether or not the game is windowed at start
+     */
     public Game(String name, boolean windowed){
         gameName = name;
-        try{
-            init();
-            //create window
-            //TODO: implement a read settings to create window(maybe a constructor?)
-            window = new Window(gameName, 600, 800, windowed);
-            
-            // Setup a key callback. It will be called every time a key is pressed, repeated or released.
-            glfwSetKeyCallback(window.getHandle(), this);
-            // Enable v-sync
-            glfwSwapInterval(1);
-            // Make the window visible
-            glfwShowWindow(window.getHandle());
-            
-            run();
-        }finally{
-            glfwTerminate();
-            errorCallback.release();
-        }
+        init();
+        //create window
+        //TODO: implement a read settings to create window(maybe a constructor?)
+        window = new Window(gameName, 600, 800, windowed);
+
+        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
+        glfwSetKeyCallback(window.getHandle(), this);
+        // Enable v-sync
+        glfwSwapInterval(1);
+        // Make the window visible
+        glfwShowWindow(window.getHandle());
+
             
         
     }
+    /**
+     * Creates a windowed Game with the passed name
+     * @param name label for game window
+     */
     public Game(String name){
         this(name, true);
     }
-    
+    /**
+     * Toggles fullscreen mode
+     */
     public void toggleFullscreen(){
         window.toggleFullscreen();
         glfwSetKeyCallback(window.getHandle(), this);
     }
-    
+    /**
+     * Initialize glfw
+     */
     private void init(){
         // initialize GLFW
         glfwSetErrorCallback(errorCallback = errorCallbackPrint(System.err));
@@ -66,10 +74,16 @@ public abstract class Game extends GLFWKeyCallback{
  
         
     }
+    /**
+     * Ends the game by telling glfw that the window should close
+     */
     public void endGame(){
         glfwSetWindowShouldClose(window.getHandle(), GL_TRUE);
     }
-    private void run(){
+    /**
+     * Runs the game and loops it
+     */
+    public void run(){
         System.out.println("Hello LWJGL " + Sys.getVersion() + "!");
         
         GLContext.createFromCurrent();
@@ -81,21 +95,33 @@ public abstract class Game extends GLFWKeyCallback{
  
            
             if(glfwGetTime()-lastTime >= (double)1/fps_limit){
-                update();
+                update(glfwGetTime()-lastTime);
                 glfwSwapBuffers(window.getHandle()); // swap the color buffers
                 lastTime = glfwGetTime();
             }
             glfwPollEvents();// Poll for window events. 
         }
+        glfwTerminate();
+        errorCallback.release();
     }
     
     
     @Override
+    /**
+     * The key listener method, calls @see interpretKeys
+     */
     public void invoke(long winHandle, int key, int scancode, int action, int mods){
         interpretKeys(key, action);
     }
-    
+    /**
+     * Interprets keys that are pressed
+     * @param key the OpenGL KeyID of the pressed key
+     * @param action the event ID
+     */
     public abstract void interpretKeys(int key, int action);
-    
-    public abstract void  update();
+    /**
+     * updates the game state
+     * @param elapsed the time elapsed since last frame
+     */
+    public abstract void  update(double elapsed);
 }
