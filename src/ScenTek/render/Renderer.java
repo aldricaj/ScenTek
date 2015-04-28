0100ba86a922f49215e222fafbc0c1814c44502b
aldrich_a <aldrich_a@WH7010Rm243-17.llsdcompscience.internal>
2015-04-28 10:48:47 -0400

    Started conversion to single Render class

--- src/ScenTek/render/Renderer.java
+++ src/ScenTek/render/Renderer.java
@@ -1,42 +1,56 @@
 
 package ScenTek.render;
 
+import ScenTek.geom.Vertex;
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL30.*;
 
 /**
  * This interface will handle rendering
  * @author aaldrich
  */
-public abstract class Renderer {
-    protected int vao = -1;// allows subclass to determine whether or not it has a created vao
+public class Renderer {
+    private int simpleVAO, simpleVBO;
     protected float[] points;
     protected byte[] indices;
-    protected Renderer(){
+    public enum RenderMode{
+        SIMPLE, TEXTURED
+    };
+    private Renderer(){
+        
+    }
+    private void initSimpleRender(){
+        simpleVAO = glGenVertexArrays();
+        simpleVBO = glGenBuffers();
         
     }
     
-    /**
-     * Load the necessary settings for the render type
-     
-     * Should contain all initialization for whatever rendering process is needed by the subclass. It'll set up VAO mainly, using the id assigned
-     */
-    protected abstract void loadSettings();
-    /**
-     * Set everything modified to it's default value
-     */
-    protected abstract void unloadSettings();
+    private void initTexturedRender(){
+        
+    }
     
     /**
      * Render at the points and the indices passed
      * @param pts points to be rendered at
      * @param indices indices for the triangles to be used
      */
-    public void render(float[] pts, byte[] indices){
-        points = pts;
-        this.indices = indices;
-        loadSettings();
+    public void render(Vertex[] vertices, RenderMode mode) throws IllegalArgumentException{
+        
+        switch(mode){
+            case SIMPLE:
+                if(vertices instanceof ColoredVertex[]){
+                    
+                }
+                else throw new IllegalArgumentException("Wrong Vertex Type Passed");
+                break;
+            case TEXTURED:
+                
+                break;
+        }
+        
         glDrawElements(GL_TRIANGLES, indices.length, GL_UNSIGNED_BYTE, 0);
-        unloadSettings();
+        
     }
     
     
